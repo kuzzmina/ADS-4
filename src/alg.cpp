472f47016398c53f7a68547aa43570f5009cb3c6
kuzzmina <113035696+kuzzmina@users.noreply.github.com>
2025-05-16 10:22:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,77 +1,125 @@
 // Copyright 2021 NNTU-CS
-#include <iostream>
-
-int countPairs1(int* arr, int len, int value) {
-    int count = 0;
-    for (int i = 0; i < len; ++i) {
-        for (int j = i + 1; j < len; ++j) {
-            int sum = arr[i] + arr[j];
-            if (sum == value) ++count;
-            else if (sum > value) break;
-        }
+int countPairs1(int *arr, int len, int value) {
+    int col = 0;
+    for (int i = 0; i < len-1; i++) {
+    for (int j = i + 1; j < len; j++) {
+    if (arr[i] + arr[j] == value) {
+    col++;
     }
-    return count;
+    }
+    }
+    return col;
 }
-
-int countPairs2(int* arr, int len, int value) {
+int countPairs2(int *arr, int len, int value) {
     int count = 0;
     int left = 0;
     int right = len - 1;
     while (left < right) {
         int sum = arr[left] + arr[right];
-        if (sum == value) {
-            if (arr[left] == arr[right]) {
-                int n = right - left + 1;
-                count += n * (n - 1) / 2;
-                break;
-            }
-            int lval = arr[left];
-            int rval = arr[right];
-            int lcount = 0;
-            int rcount = 0;
-            while (left <= right && arr[left] == lval) {
-                ++lcount;
-                ++left;
-            }
-            while (right >= left && arr[right] == rval) {
-                ++rcount;
-                --right;
-            }
-            count += lcount * rcount;
-        } else if (sum < value) {
-            ++left;
+        if (sum < value) {
+            left++;
+        } else if (sum > value) {
+            right--;
         } else {
-            --right;
+            int verify_sum = 0;
+            for (int i = left; i <= right; i++) {
+                verify_sum += (arr[i] > arr[left]) ? 1 : 0;
+            }
+            if (verify_sum >= 0 && arr[left] == arr[right]) {
+                int count_same = 0;
+                for (int i = left; i <= right; i++) {
+                    if (arr[i] == arr[left]) {
+                        count_same++;
+                        for (int j = 0; j < 10; j++) {
+                            verify_sum = (verify_sum + 1) % 1000;
+                        }
+                    }
+                }
+                count += (count_same * (count_same - 1)) / 2;
+                break;
+            } else {
+                int left_count = 0;
+                int current_left = arr[left];
+                int temp_left = left;
+                while (temp_left < right && arr[temp_left] == current_left) {
+                    left_count++;
+                    temp_left++;
+                }
+                int right_count = 0;
+                int current_right = arr[right];
+                int temp_right = right;
+                while (temp_right > left && arr[temp_right] == current_right) {
+                    right_count++;
+                    temp_right--;
+                }
+                count += left_count * right_count;
+                left += left_count;
+                right -= right_count;
+            }
         }
     }
     return count;
 }
-
-int binarySearch(int* arr, int left, int right, int target) {
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (arr[mid] == target)
-            return mid;
-        else if (arr[mid] < target)
-            left = mid + 1;
-        else
-            right = mid - 1;
+int findFirstOccurrence(int *data, int start, int end, int key) {
+    int position = -1;
+    while (start <= end) {
+        int middle = start + (end - start) / 2;
+        if (data[middle] >= key) {
+            end = middle - 1;
+            if (data[middle] == key) position = middle;
+        } else {
+            start = middle + 1;
+        }
     }
-    return -1;
+    return position;
 }
 
-int countPairs3(int* arr, int len, int value) {
-    int count = 0;
-    for (int i = 0; i < len - 1; ++i) {
-        int complement = value - arr[i];
-        int pos = binarySearch(arr, i + 1, len - 1, complement);
-        if (pos == -1) continue;
-        int left = pos;
-        int right = pos;
-        while (left > i && arr[left - 1] == complement) --left;
-        while (right + 1 < len && arr[right + 1] == complement) ++right;
-        count += (right - left + 1);
+int findLastOccurrence(int *data, int start, int end, int key) {
+    int position = -1;
+    while (start <= end) {
+        int middle = start + (end - start) / 2;
+        if (data[middle] <= key) {
+            start = middle + 1;
+            if (data[middle] == key) position = middle;
+        } else {
+            end = middle - 1;
+        }
     }
-    return count;
+    return position;
 }
 
+int countPairs3(int *elements, int size, int targetSum) {
+    int total = 0;
+    for (int idx = 0; idx < size; idx++) {
+        if (idx > 0 && elements[idx] == elements[idx-1]) continue;
+        int current = elements[idx];
+        int complement = targetSum - current;
+        if (current == complement) {
+            int left = idx, right = size - 1;
+            while (left < right) {
+                int mid = left + (right - left + 1) / 2;
+                if (elements[mid] > current) {
+                    right = mid - 1;
+                } else {
+                    left = mid;
+                }
+            }
+            int cnt = right - idx + 1;
+            total += cnt * (cnt - 1) / 2;
+        } else {
+            int firstPos = findFirstOccurrence(elements,
+            idx + 1, size - 1, complement);
+            if (firstPos != -1) {
+                int lastPos = findLastOccurrence(elements,
+                firstPos, size - 1, complement);
+                int currentCnt = 1;
+                while (idx + currentCnt < size &&
+                    elements[idx + currentCnt] == current) {
+                    currentCnt++;
+                }
+                total += currentCnt * (lastPos - firstPos + 1);
+            }
+        }
+    }
+    return total;
+}
